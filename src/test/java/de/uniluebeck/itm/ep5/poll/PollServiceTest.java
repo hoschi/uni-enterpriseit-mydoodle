c21369704eab819f3629a68943b5d7b75ef62d52
Stefan Gojan <vollekannehoschi@gmail.com>
2010-12-05 17:05:23 +0100

    [#6601201] added new tests ant todos

--- src/test/java/de/uniluebeck/itm/ep5/poll/PollServiceTest.java
+++ src/test/java/de/uniluebeck/itm/ep5/poll/PollServiceTest.java
@@ -2,7 +2,7 @@ package de.uniluebeck.itm.ep5.poll;
 
 //import static org.hamcrest.CoreMatchers.equalTo;
 //import static org.junit.Assert.assertThat;
-
+import java.util.List;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,40 +12,138 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import de.uniluebeck.itm.ep5.poll.domain.Option;
 import de.uniluebeck.itm.ep5.poll.domain.Poll;
 import de.uniluebeck.itm.ep5.poll.service.PollService;
+import org.junit.Assert;
 
 public class PollServiceTest {
 
-	final static Logger logger = LoggerFactory.getLogger(PollServiceTest.class);
+    final static Logger logger = LoggerFactory.getLogger(PollServiceTest.class);
 
-	@Test
-	public void simplePoll() {
-		// Create the spring container using the XML configuration in
-		// application-context.xml
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"application-context.xml");
+    /*
+     * nutzer kann abstimmung anlegen
+     */
+    @Test
+    public void addPoll() {
+        // Create the spring container using the XML configuration in
+        // application-context.xml
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "application-context.xml");
 
-		// Retrieve the beans we'll use during testing
-		PollService pollService = ctx.getBean(PollService.class);
+        // Retrieve the beans we'll use during testing
+        PollService pollService = ctx.getBean(PollService.class);
 
-		// Create options to test
-		Option wine = new Option("Wine", "red and tasty", 0);
-		Option beer = new Option("Beer", "cold and tasty", 0);
-		Option whiskey = new Option("Whiskey", "strong and tasty", 0);
+        Poll poll = new Poll("createpoll");
+        pollService.addPoll(poll);
 
-		// Create voting to test
-		Poll poll = new Poll("Which drink?");
-		poll.addOption(wine);
-		poll.addOption(beer);
-		poll.addOption(whiskey);
-		pollService.addPoll(poll);
+        List<Poll> list = pollService.getPolls();
+        Assert.assertEquals(1, list.size());
 
-		// Print all polls and options
-		for (Poll p : pollService.getPolls()) {
-			logger.info(p.toString());
-			for (Option o : p.getOptions()) {
-				logger.info(o.toString());
-			}
-		}
+        // Print all polls and options
+        for (Poll p : pollService.getPolls()) {
+            logger.info(p.toString());
+        }
+    }
 
-	}
+    /*
+     * beim anlegen einer abstimmung wird eine eindeutige ID angeleget
+     */
+    @Test
+    public void createPollId() {
+        // Create the spring container using the XML configuration in
+        // application-context.xml
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "application-context.xml");
+
+        // Retrieve the beans we'll use during testing
+        PollService pollService = ctx.getBean(PollService.class);
+
+        Poll poll = new Poll("identity");
+        pollService.addPoll(poll);
+
+        poll = new Poll("identity1");
+        pollService.addPoll(poll);
+        List<Poll> list = pollService.getPolls();
+        Assert.assertEquals(2, list.size());
+
+        int id = list.get(0).getId();
+        int id1 = list.get(1).getId();
+        Assert.assertFalse(id == id1);
+
+        // Print all polls and options
+        for (Poll p : pollService.getPolls()) {
+            logger.info(p.toString());
+        }
+    }
+
+    /////////////////////////////////////////////////////
+    // TODO _nur_ anleger kann eine angelegte abstimmung verwalten (CRUD)
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO nutzer kann angbeben wie lange die abstimmung aktiv ist
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO nutzer kann bestimmen ob die abstimmung public
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO abstimmung kann beliebigviele optionlisten enthalten
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO abstimmung kann beliebigviele optionen enthalten
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO nutzer kann also option datum oder text angeben
+    /////////////////////////////////////////////////////
+
+    /*
+     * TODO nutzer kann datums und frei text option anlegen
+     */
+    @Test
+    public void createDateAndTextOptions() {
+        // Create the spring container using the XML configuration in
+        // application-context.xml
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "application-context.xml");
+
+        // Retrieve the beans we'll use during testing
+        PollService pollService = ctx.getBean(PollService.class);
+
+        // Create options to test
+        Option wine = new Option("Wine", "red and tasty", 0);
+        Option beer = new Option("Beer", "cold and tasty", 0);
+        Option whiskey = new Option("Whiskey", "strong and tasty", 0);
+        // Create voting to test
+        Poll poll = new Poll("optiontest");
+        poll.addOption(wine);
+        poll.addOption(beer);
+        poll.addOption(whiskey);
+        pollService.addPoll(poll);
+        List<Poll> list = pollService.getPolls();
+        for (Poll p : list) {
+            if (p.getTitle().equals("optiontest")) {
+                Assert.assertEquals(3, p.getOptions().size());
+            }
+        }
+
+        // Print all polls and options
+        for (Poll p : pollService.getPolls()) {
+            logger.info(p.toString());
+            for (Option o : p.getOptions()) {
+                logger.info(o.toString());
+            }
+        }
+    }
+    /////////////////////////////////////////////////////
+    // TODO optionen können in verschiedenen sprachen eingeben un angezeigt werden
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO nutzer kann abstimmen in dem er seinen namen angbibt und seine gewählten optionen
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO interaktive abstimmungen können eingesehen werden
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO interaktive abstimmungen können nicht mehr verändert werden
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // TODO nutzer kann abstimmungen nach titel suchen mit wildcards
+    /////////////////////////////////////////////////////
 }

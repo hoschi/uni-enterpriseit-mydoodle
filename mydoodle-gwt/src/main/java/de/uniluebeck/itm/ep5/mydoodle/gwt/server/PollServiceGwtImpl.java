68c24c6a45150d99ae6921d32fe28038ce2730e2
Stefan Gojan <vollekannehoschi@gmail.com>
2011-01-27 20:40:20 +0100

    connection to web service work

--- mydoodle-gwt/src/main/java/de/uniluebeck/itm/ep5/mydoodle/gwt/server/PollServiceGwtImpl.java
+++ mydoodle-gwt/src/main/java/de/uniluebeck/itm/ep5/mydoodle/gwt/server/PollServiceGwtImpl.java
@@ -6,6 +6,15 @@ package de.uniluebeck.itm.ep5.mydoodle.gwt.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import de.uniluebeck.itm.ep5.mydoodle.gwt.client.PollServiceGwt;
+import de.uniluebeck.itm.pollservice.Pollservice;
+import de.uniluebeck.itm.pollservice.XsPollInfo;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.namespace.QName;
 
 /**
  *
@@ -14,6 +23,13 @@ import de.uniluebeck.itm.ep5.mydoodle.gwt.client.PollServiceGwt;
 public class PollServiceGwtImpl extends RemoteServiceServlet implements PollServiceGwt {
 
 	public String myMethod(String s) {
-		return "foooo you " + s;
+		Pollservice instance = null;
+		try {
+			instance = new Pollservice(new URL("http://localhost:8080/poll?WSDL"), new QName("www.itm.uniluebeck.de/pollservice", "Pollservice"));
+		} catch (MalformedURLException ex) {
+			Logger.getLogger(PollServiceGwtImpl.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		List<XsPollInfo> polls = instance.getPollWebServicePort().getPolls(Locale.ENGLISH.toString());
+		return "size " + polls.size();
 	}
 }
